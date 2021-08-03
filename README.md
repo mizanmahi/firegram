2b3a71d7a731153b78e39e2d9b3e62d5d31fa821
mizanmahi <mizanmahi24@gmail.com>
2021-08-03 14:41:10 +0600

    add: live url

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+-See Live Here: [firegram](https://firegram-by-mahi.netlify.app/)
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -10,3 +12,5 @@ Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 #### Simple instagram like gallery 
+
+### Mizanur Rahman
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -30,7 +30,6 @@ const ImageGrid = ({ setSelectedImage }) => {
 
     }).catch((error) => {
       // Uh-oh, an error occurred!
-      console.log(`oops! error deleting image`);
     });
 
   }
@@ -38,14 +37,25 @@ const ImageGrid = ({ setSelectedImage }) => {
   return (
     <div className="img-grid">
       {docs && docs.map(doc => {
-        return (<motion.div className="img-wrap" key={doc.id} onClick={(e) => clickHandler(e, doc.imageUrl)} layout>
-          <motion.img src={doc.imageUrl} alt="uploaded img" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: .5 }} />
-          <span className="dlt-btn" onClick={(e) => dltHendler(e, doc.name, doc.id)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-            </svg>
-          </span>
-        </motion.div>)
+        return (
+          <motion.div
+            className="img-wrap"
+            key={doc.id}
+            onClick={(e) => clickHandler(e, doc.imageUrl)}
+            layout>
+            <motion.img
+              src={doc.imageUrl}
+              alt="uploaded img"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: .5 }} />
+            <span className="dlt-btn" onClick={(e) => dltHendler(e, doc.name, doc.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+              </svg>
+            </span>
+          </motion.div>
+        )
       })}
 
     </div>
